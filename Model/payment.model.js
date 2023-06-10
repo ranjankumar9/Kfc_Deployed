998097a1f69a58b6728105bba5cf7f906c02fc6b
Ranjan Kumar <107936455+ranjankumar9@users.noreply.github.com>
2023-06-10 06:09:23 +0530

    Update payment.model.js

--- Model/payment.model.js
+++ Model/payment.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const PaymentSchema = mongoose.Schema({
     cardnumber:{type:Number, required:true},
-    expirydate:{type:Number, required:true},
+    expirydate:{type:String, required:true},
     cvv:{type:Number, required:true}
 },{
     versionKey:false
